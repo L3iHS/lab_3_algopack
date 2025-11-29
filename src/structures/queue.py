9f94a5bdbe608c951c946cbd3d39441fb652843f
L3iHS <ilyadroscov@gmail.com>
2025-11-29 18:40:10 +0300

    add ListQueue

--- src/structures/queue.py
+++ src/structures/queue.py
@@ -1,5 +1,27 @@
 class ListQueue:
-    pass
+    def __init__(self):
+        self._data: list[int] = []
+    
+    def is_empty(self) -> bool:
+        return len(self._data) == 0
+    
+    def __len__(self) -> int:
+        return len(self._data)
+    
+    def enqueue(self, x: int) -> None:
+        self._data.append(x)
+    
+    def dequeue(self) -> int:
+        if self.is_empty():
+            raise IndexError('Очередь пуста')
+        return self._data.pop(0)
+    
+    def front(self) -> int:
+        if self.is_empty():
+            raise IndexError('Очередь пуста')
+        return self._data[0]
+    
+
 
 class LinkedListQueue:
     pass
